241b6c2cc3ddb4513deee3290e442a7755e97078
Andy Lee <freerangeandy@gmail.com>
2020-04-22 17:25:00 -0400

    games index tile stub

---
+++ app/javascript/react/components/GameIndexTile.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const GameIndexTile = props => {
+   const testGameTitle = props.title
+
+    return(
+        <div>
+            <h4> {testGameTitle} </h4>
+        </div>
+    )
+}
+
+export default GameIndexTile
--- app/javascript/react/containers/GamesIndexContainer.js
+++ app/javascript/react/containers/GamesIndexContainer.js
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from "react"
 
+import GameIndexTile from "../components/GameIndexTile"
+
 const GamesIndexContainer = props => {
+    const testGamesList = [{title: "Knack"}, {title: "Sekiro: Shadows Die Twice"}, {title: "Death Stranding"}, {title: "Civilization"}, {title: "Mario Kart"}, {title: "Catan Universe"} ]
+    const gameTileList = testGamesList.map((game, index) => {
+        return(
+            <GameIndexTile 
+                key={index}
+                title={game.title}
+            />
+        )
+     })
+     
     return(
-        <div> Hello from GamesIndexContainer </div>
+        <div>
+            {gameTileList}
+        </div>
     )
 }
 
