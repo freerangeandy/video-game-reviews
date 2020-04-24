2004ef019cd52c662675480789aa78b05c06b5d1
BenjaminFairbank <BenFairbank26@gmail.com>
2020-04-24 16:45:31 -0400

    new game form created in games index

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,4 +2,20 @@ class Api::V1::GamesController < ApplicationController
   def index
     render json: Game.all
   end
+
+  def create
+    game = Game.new(game_params)
+
+    if game.save
+      render json: game
+    else
+      render json: {error: "Game did not save"}
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :description, :creater, :platform, :genre, :site, :release_date)
+  end
 end
--- app/javascript/react/containers/GamesIndexContainer.js
+++ app/javascript/react/containers/GamesIndexContainer.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react"
 
 import GamesIndexPane from "../components/GamesIndexPane"
+import NewGameFormComponent from "../components/NewGameFormComponent"
 
 const GamesIndexContainer = props => {
   const [games, setGames] = useState([])
-
+  
   useEffect(() => {
     fetch("/api/v1/games.json")
     .then((response) => {
@@ -23,9 +24,40 @@ const GamesIndexContainer = props => {
     .catch(error => console.error(`Error in fetch: ${error.message}`))
   }, [])
 
+  const fetchPostNewGame = (gamePayload) => {
+    fetch("/api/v1/games", {
+      credentials: "same-origin",
+      method: "POST",
+      body: JSON.stringify(gamePayload),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage)
+        throw error
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      let game = body
+      setGames([
+        ...games,
+        game
+      ])
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }
+
   return (
     <div className="grid-container game-index-margin">
       <GamesIndexPane gamesList={games} />
+      <NewGameFormComponent fetchPostNewGame={fetchPostNewGame} />
     </div>
   )
 }
