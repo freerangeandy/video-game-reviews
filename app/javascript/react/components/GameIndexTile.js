c4db2f0fc20e0695582aa7c33cb8bd3ad26862b2
BenjaminFairbank <BenFairbank26@gmail.com>
2020-04-29 17:54:48 -0400

    Changes by nick request. more styling progress on the show page.

--- app/javascript/react/components/GameIndexTile.js
+++ app/javascript/react/components/GameIndexTile.js
@@ -2,15 +2,14 @@ import React from "react"
 import { Link } from 'react-router-dom'
 
   const GameIndexTile = props => {
+    const id = props.game.id
 
-    let src = ''
+    let src
     if(props.game.image) {
       src = props.game.image
     } else {
       src = "https://store-images.s-microsoft.com/image/apps.18145.14148865384803760.43665508-e225-4b8e-95bd-232694841824.392de630-5f87-44a6-8031-b1c503929ffb?mode=scale&q=90&h=1080&w=1920"
     }
-
-    const id = props.game.id
 
     return (
       <Link to={`games/${id}`} className="cell small-4 game-tile">
