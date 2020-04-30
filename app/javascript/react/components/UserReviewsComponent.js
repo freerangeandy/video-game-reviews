4b8b422e0e7101422270528c43e767136eaac601
BenjaminFairbank <BenFairbank26@gmail.com>
2020-04-30 18:55:31 -0400

    review tiles mapping/fetch

--- app/javascript/react/components/UserReviewsComponent.js
+++ app/javascript/react/components/UserReviewsComponent.js
@@ -1,12 +1,52 @@
 import React, { Fragment } from "react"
 
 const UserReviewsComponent = props => {
+  
+  const fetchDeleteReview = (reviewID) => {
+    fetch(`/api/v1/reviews/${reviewID}`, {
+      credentials: "same-origin",
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+           error = new Error(errorMessage)
+        throw error
+      }
+    })
+    .then(response => response.json())
+    .then(game => {
+      setReviews(game.reviews)
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }
+
+  let allReviews = null
+  if (props.reviews.length > 0) {
+    allReviews = props.reviews.map((review) => {
+      return(
+        <ReviewIndexTile
+          key={review.id}
+          id={review.id}
+          rating={review.rating}
+          comment={review.comment}
+          fetchDeleteReview={fetchDeleteReview}
+        />
+      )
+    })
+  }
 
   return (
     <>
       <div className="user-reviews">
         <h3>You have written {props.reviews.length} reviews!</h3>
-        {props.reviews}
+        {allReviews}
       </div>
     </>
   )
